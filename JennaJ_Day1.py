53ce9ec68e6820f63f36d47a59134145347ba680
jennajeletic <53440626+jennajeletic@users.noreply.github.com>
2019-07-29 13:55:03 -0400

    Add files via upload

--- JennaJ_Day1.py
+++ JennaJ_Day1.py
@@ -14,4 +14,24 @@ print("Hello, my name is Jenna")
 (8/9)*3
 
 print("5*2")
- 
+
+def multiply(a,b):
+    multiply = a*b
+    
+    print(multiply)
+    
+multiply(4,5)
+
+def add(a,b,c):
+    add = a+b+c
+    
+    print(add)
+    
+add(4,5,6)
+
+def areatri (b,h):
+    areatri = (1/2)*b*h
+    print("The area of the triangle with base", b, "and hieght", h, "is", areatri)
+    
+areatri(2,4)
+    
